339cd6ab27aa1671c5e195c13ac8e7a30c112b17
luof <523274366@qq.com>
2022-03-19 23:45:41 +0800

    [learn]python多线程

--- 6_approaches_and_threads/2_thread_sync.py
+++ 6_approaches_and_threads/2_thread_sync.py
@@ -19,11 +19,13 @@
         set（）：标记设置为True
         clear（）：标记设置为False
         is_set（）：标记是否为True
-        wait（timeout=None）：设置等等标记为True的时长，None为无线等待。等到返回True。未等到超时了返回Fa
+        wait（timeout=None）：设置等等标记为True的时长，None为无线等待。等到返回True。未等到超时了返回False
+        ***使用场景：老板雇佣了一个工人，等工人生产杯子。---等待工作线程完成，实现线程间通信
 
     二、threading.Lock类: 锁，凡是存在共享资源争抢的地方都可以使用锁，从而保证只有一个使用者可以完全使用这个资源。
         当前线程在读共享的资源的时候，锁住，让其他S线程看不了.
-        获取：lock.acquire（）
+        获取：lock.acquire（blocking=True, timeout=-1），成功获取锁，返回True，否则返回False
+            blocking=True:默认阻塞锁，阻塞可以设置超时时间。非阻塞锁blocking=False，禁止设置timeout
             锁可以解决共享资源，但要特别注意死锁。死锁：所有线程都在等释放，使用锁的过程中，导致所有线程者
         释放：lock.release（）
             即使任务完成，退出，退出之前也要释放。否则其他线程一直在获取锁，一直卡死
@@ -32,16 +34,74 @@
         每个线程生产10个，作为局部变量返回。但是有的线程忙，一生产不完怎么办？即木桶原理
 
         加锁、解锁语句：
-            加锁和解锁之间的代码实现有可能因为异常，线程异常退出，而未解锁，导致死锁。因此，加锁解锁。
+            加锁和解锁之间的代码实现有可能因为异常，线程异常退出，而未解锁，导致死锁。因此，加解锁非常重要。
             1、使用try．．．finally．．．语句，保证锁释放
             2、with上下文管理，锁对象支持上下文管理。有enter和exist魔术方法的对象都支持上下文管理
+
+        ***应用场景：适用于访问和修改同一个共享资源的时候，即读写同一个资源的时候。如果全都是读取同一个资源不需要锁
+
         注意事项：
             1、少用锁，必要时用锁。使用了锁，多线程访问被锁的资源时，就成了串行，要么排队执行，要么争抢执
             2、加锁时间越短越好，不需要就立即释放
             3、一定要避免死锁
+    三、threading.Rlock类：可重入锁,是线程相关的锁。
+        线程A获得可重复锁，并可以多次成功获取，不会阻塞。最后要在线程A中做和acquire次数相同的release。
+        获取：threading.Rlock().acquire()
+        释放：threading.Rlock.release()
+
+    四、threading.Condition(lock=None)类：可传入一个Lock或Rlock对象，不传默认是RLock
+        方法：
+        acquire():获取锁
+        wait(timeout=None):可设置超时时间
+        notify(n=1)：唤醒至多指定数目个线程，没有等待的线程就不做任何操作
+        notify_all()：唤醒所有等待的线程
+        ***使用场景：用于生产者、消费者模型，为了解决生产者和消费者速度匹配同步问题。
+        注意：因为Condition内部默认使用了Rlock，因此必须先acquire，用完要release。最好使用with上下文。
+             消费者wait；生产者生产好消息后，对消费者发通知，使用notify或notify_all方法
+        一个生产者对多个消费者时，实现了消息的一对多，这其实就是广播。
+        本例只做演示用，线程不安全.消费者再读的时候可能生产者又在生产新的
+
+    五、threading.Barrier(parties, action=None, timeout=None)类：栅栏、屏障。
+        理解：将线程分组，parities为一组，当达到parities个时，放行，循环下一组进入等待
+        n_waiting属性: 返回栅栏中处于等待的线程数
+        parities: 指定的等待数目
+        wait(timeout=None)方法:设置等待,返回0-parities数字。可以设置超时时间，超时后栅栏被打破abort，等待中的线程或调用等待方法的线程中，
+                           都会抛出BrokenBarrierError异常，直到reset方法恢复栅栏
+        broken属性：如果栅栏处于打破状态，返回True
+        abort()方法：打破栅栏
+        reset()方法：恢复栅栏
+
+        执行逻辑：所有线程冲到barrier前等待，直到达到parities数目的线程，栅栏打开，所有线程继续执行。再有线程wait，继续循环
+        例如：赛马比赛，所有马匹达到栅栏就位，开闸放马，再关闸。下一批马匹陆续就位后再开闸
+
+        应用：并发初始化。所有线程都必须初始化完成后，才能继续工作。例如：运行程序前，加载数据、检查，如果这些工作没完成，就开始运行将不能正常工作。
+            又如：启动一个程序，需要先加载磁盘文件、缓存预热、初始化连接池等工作，这些工作齐头并进，不过只有都准备好了，程序才能继续向后执行；
+            假设数据库连接失败，则初始化工作失败，就要abort，屏障broken，所有线程收到异常退出
+
+    六、信号量：
+        threading.Semaphore(value=1):信号量，类似锁，信号量对象内部维护一个倒计数器，每acquire一次，减1，当acquire方法发现计数器为0时，
+                                     就阻塞请求的线程，直到其他线程对信号量release后，计数大于0，恢复阻塞的线程。
+            acquire()方法：获取信号量，计数器减1，获取成功返回True
+            release()方法：释放信号量，计数器加1
+            计数器永远不会低于0，acquire的时候发现是0，都会被阻塞
+        应用场景：使用信号量semaphore解决资源有限的问题。
+            实例：创建连接池
+            池有最多连接数
+            可以从池中取连接
+            连接返回到池
+            线程安全
+
+        threading.BoundedSemaphore(value=1): 有界信号量
+            应用：Semaphore类，在未acquire，直接release时，会超上界。BoundedSemaphore，不允许超出初始值范围，否则抛出ValueError异常
+
+    信号量和锁：
+        锁，只允许同一时间一个线程独占资源。它是特殊的信号量，即信号量计数器初值为1.
+        信号量，允许多个线程访问共享资源，但这个共享资源数量有限
+        锁可以看做特殊的信号量
 
 """
 import datetime
+import random
 import threading
 import time
 
@@ -187,16 +247,293 @@ def run_counter(c: CounterLockDemo, count=100):
                 c.inc()
 
 
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.lock = threading.Lock()
+
+
+class NotBlockLockDemo:
+    """
+    构造10个任务，即10把非阻塞锁，起5个线程去争抢任务。通过time.sleep强制线程切换，且获取到锁的线程不释放锁，演示非阻塞锁争抢到任务的不确定性
+    """
+    def __init__(self):
+        self.task_lst = [Task('task-{}'.format(x)) for x in range(10)]
+
+    def worker(self):
+        for task in self.task_lst:
+            time.sleep(0.01)
+            if task.lock.acquire(False):
+                logger.info('{} {} begin to start'.format(threading.current_thread(), task.name))
+            else:
+                logger.info('{} {} is working'.format(threading.current_thread(), task.name))
+
+    def run_thread(self):
+        for i in range(5):
+            threading.Thread(target=self.worker, name='worker={}'.format(i)).start()
+
+
+def sub(lock: threading.RLock):
+    """
+    可重入锁，获取了几次，就要释放几次，多释放会报错
+    :param lock:
+    :return:
+    """
+    lock.acquire()
+    lock.acquire()
+    lock.release()
+    lock.release()
+    lock.release()
+
+
+class Dispatcher:
+    """
+    threading.Condition(lock=None)类：可传入一个Lock或Rlock对象，不传默认是RLock
+        方法：
+        acquire():获取锁
+        wait(timeout=None):可设置超时时间
+        notify(n=1)：唤醒至多指定数目个线程，没有等待的线程就不做任何操作
+        notify_all()：唤醒所有等待的线程
+        ***使用场景：用于生产者、消费者模型，为了解决生产者和消费者速度匹配同步问题。
+        注意：因为Condition内部默认使用了Rlock，因此必须先acquire，用完要release。最好使用with上下文。
+             消费者wait；生产者生产好消息后，对消费者发通知，使用notify或notify_all方法
+        一个生产者对多个消费者时，实现了消息的一对多，这其实就是广播。
+        本例只做演示用，线程不安全.消费者再读的时候可能生产者又在生产新的
+    """
+    def __init__(self):
+        # 生产者线程、消费者线程
+        # 生产者生产数字完成后，叫醒消费者；消费者排队等待产品，进入等待池
+        # 数字要被上锁
+        self.con = threading.Condition()
+        self.ev = threading.Event()
+        self.num = None
+
+    def produce(self):
+        # while not self.ev.is_set():
+        for _ in range(10):
+            with self.con:
+                self.num = random.randrange(0, 100)
+                logger.info('{} make the num:{}'.format(threading.current_thread().name, self.num))
+                self.con.notify(2)  # 生产者生产完成，通知等待池的线程取产品
+            self.ev.wait(1)  # 模拟生产速度
+        logger.info('{} product over'.format(threading.current_thread().name))
+
+    def costumer(self):
+        while not self.ev.is_set():
+            with self.con:
+                self.con.wait()  # 消费者排队等产品，进入等待线程池
+                # 当生产者使用while语句时，此处线程不安全，例如，当线程进入wait池，但是在这之前，执行了self.ev.set()，则生产者不获取锁，直接停止生产，wait池中的线程一直等待产品,进入死锁
+                logger.info("{} get the num:{}".format(threading.current_thread().name, self.num))
+                self.num = None
+            self.ev.wait(0.5)  # 模拟消费速度
+        logger.info('{} consumer completed'.format(threading.current_thread().name))
+
+    def set_ev(self):
+        self.ev.set()
+        logger.info('finish')
+
+
+class BarrierDemo:
+    """
+    threading.Barrier(parties, action=None, timeout=None)类：栅栏、屏障。
+        理解：将线程分组，parities为一组，当达到parities个时，放行，循环下一组进入等待
+        n_waiting属性: 返回栅栏中处于等待的线程数
+        parities: 指定的等待数目
+        wait(timeout=None)方法:设置等待,返回0-parities数字。可以设置超时时间，超时后栅栏被打破abort，等待中的线程或调用等待方法的线程中，
+                           都会抛出BrokenBarrierError异常，直到reset方法恢复栅栏
+        broken属性：如果栅栏处于打破状态，返回True
+        abort()方法：打破栅栏
+        reset()方法：恢复栅栏
+
+        执行逻辑：所有线程冲到barrier前等待，直到达到parities数目的线程，栅栏打开，所有线程继续执行。再有线程wait，继续循环
+        例如：赛马比赛，所有马匹达到栅栏就位，开闸放马，再关闸。下一批马匹陆续就位后再开闸
+
+        应用：并发初始化。所有线程都必须初始化完成后，才能继续工作。例如：运行程序前，加载数据、检查，如果这些工作没完成，就开始运行将不能正常工作。
+            又如：启动一个程序，需要先加载磁盘文件、缓存预热、初始化连接池等工作，这些工作齐头并进，不过只有都准备好了，程序才能继续向后执行；
+            假设数据库连接失败，则初始化工作失败，就要abort，屏障broken，所有线程收到异常退出
+
+    """
+    @staticmethod
+    def worker(barrier: threading.Barrier, x: int):
+        logger.info('waiting for {} threads'.format(barrier.n_waiting))
+        try:
+            logger.info('Broken status:{}'.format(barrier.broken))
+            if x < 3:
+                barrier_id = barrier.wait(1)
+            else:
+                if x == 6:
+                    barrier.reset()
+                barrier_id = barrier.wait()
+            logger.info('after barrier:{}'.format(barrier_id))
+        except threading.BrokenBarrierError as e:
+            logger.error('broken barrier, {}'.format(e))
+
+    def run_barrier(self):
+        barrier = threading.Barrier(3)
+        for i in range(9):
+            # if i == 2:
+            #     barrier.abort()
+            # elif i == 6:
+            #     barrier.reset()
+            threading.Event().wait(2)
+            threading.Thread(target=self.worker, args=(barrier, i), name='worker_{}'.format(i)).start()
+
+
+class SemaphoreDemo:
+    @staticmethod
+    def worker(s: threading.Semaphore):
+        logger.info('in sub')
+        s.acquire()
+        logger.info('end sub')
+
+    def run_s(self):
+        s = threading.Semaphore(3)
+        logger.info(s.acquire())
+        logger.info(s.acquire())
+        logger.info(s.acquire())
+        logger.info('----------------')
+        logger.info(s.acquire(False))
+        logger.info(s.acquire(timeout=3))
+        threading.Thread(target=self.worker, args=(s,)).start()
+        s.release()
+
+
+class Conn:
+    def __init__(self, name):
+        self.name = name
+
+
+class ConnPoolLockDemo:
+    """
+    使用锁方法 创建一个连接池
+    限制：
+        池有最多连接数
+        可以从池中取连接
+        连接返回到池
+        线程安全
+    """
+    def __init__(self, count):
+        self.count = count
+        self.lock = threading.Lock()
+        self.pool = [Conn('pool_{}'.format(i)) for i in range(count)]
+
+    def get_conn(self):
+        self.lock.acquire()
+        if self.pool:
+            res_conn = self.pool.pop()
+            self.lock.release()
+            return res_conn
+        return None
+
+    def return_conn(self, conn):
+        self.lock.acquire()
+        if len(self.pool) < self.count:
+            self.pool.append(conn)
+            self.lock.release()
+        else:
+            logger.warning('exceed the threshold value: {}'.format(self.count))
+
+
+class ConnPoolSemaphoreDemo:
+    """
+    信号量：
+        threading.Semaphore(value=1):信号量，类似锁，信号量对象内部维护一个倒计数器，每acquire一次，减1，当acquire方法发现计数器为0时，
+                                     就阻塞请求的线程，直到其他线程对信号量release后，计数大于0，恢复阻塞的线程。
+            acquire()方法：获取信号量，计数器减1，获取成功返回True
+            release()方法：释放信号量，计数器加1
+            计数器永远不会低于0，acquire的时候发现是0，都会被阻塞
+        应用场景：使用信号量semaphore解决资源有限的问题。
+            实例：创建连接池
+            池有最多连接数
+            可以从池中取连接
+            连接返回到池
+            线程安全
+
+        threading.BoundedSemaphore(value=1): 有界信号量
+            应用：Semaphore类，在未acquire，直接release时，会超上界。BoundedSemaphore，不允许超出初始值范围，否则抛出ValueError异常
+
+    信号量和锁：
+        锁，只允许同一时间一个线程独占资源。它是特殊的信号量，即信号量计数器初值为1.
+        信号量，允许多个线程访问共享资源，但这个共享资源数量有限
+        锁可以看做特殊的信号量
+
+    """
+    def __init__(self, count):
+        self.count = count
+        self.sema = threading.Semaphore(3)
+        self.pool = [Conn('pool_{}'.format(i)) for i in range(count)]
+
+    def get_conn(self):
+        self.sema.acquire()
+        res_conn = self.pool.pop()
+        return res_conn
+
+    def return_conn(self, conn):
+        self.pool.append(conn)
+        self.sema.release()
+
+
+class CanUesConnPoolSemaphoreDemo:
+    def __init__(self, count: int):
+        self.count = count
+        self.sem = threading.BoundedSemaphore(count)
+        self.conn_pool = [Conn('conn_{}'.format(i)) for i in range(count)]
+
+    def get_conn(self):
+        self.sem.acquire()
+        return self.conn_pool.pop()
+
+    def return_conn(self, conn):
+        """
+        异常场景：线程A/B/C都执行到第一句append，利用BoundedSemaphore超界抛ValueError异常，使用try...except...
+        """
+        try:
+            self.conn_pool.append(conn)
+            self.sem.release()
+        except ValueError:
+            self.conn_pool.pop(conn)
+
+
+class RunPoolSemaphore:
+    def __init__(self):
+        self.pool = ConnPoolSemaphoreDemo(3)
+
+    @staticmethod
+    def semaphore_worker(pool: ConnPoolSemaphoreDemo):
+        conn = pool.get_conn()
+        logger.info(conn)
+        # 模拟使用了一段时间
+        threading.Event().wait(random.randint(1, 4))
+        pool.return_conn(conn)
+
+    def run_worker(self):
+        for i in range(6):
+            threading.Thread(target=self.semaphore_worker, args=(self.pool,)).start()
+
+
 if __name__ == '__main__':
     # CupDemo().run_demo()
     # TimerDemo(1, lambda x, y: x+y, 3, 4).start()
     # LockDemo(cup_num=100, workers=10).run_product()
-    co = CounterLockDemo(threading.Lock())
-    for i in range(10):
-        threading.Thread(target=run_counter, args=(co,)).start()
-    while True:
-        if threading.active_count() == 1:
-            logger.info('thread:{},res:{}'.format(threading.enumerate(), co.value))
-            break
-        logger.info('current value:{}'.format(co.value))
-
+    # co = CounterLockDemo(threading.Lock())
+    # for i in range(10):
+    #     threading.Thread(target=run_counter, args=(co,)).start()
+    # while True:
+    #     if threading.active_count() == 1:
+    #         logger.info('thread:{},res:{}'.format(threading.enumerate(), co.value))
+    #         break
+    #     logger.info('current value:{}'.format(co.value))
+    # NotBlockLockDemo().run_thread()
+    # disp = Dispatcher()
+    # threading.Thread(target=disp.produce, name='produce').start()
+    # threading.Thread(target=disp.costumer, name='costumer1').start()
+    # threading.Thread(target=disp.costumer, name='costumer2').start()
+    # time.sleep(3)
+    # disp.set_ev()
+    # BarrierDemo().run_barrier()
+    # SemaphoreDemo().run_s()
+    # RunPoolSemaphore().run_worker()
+    sem = threading.Semaphore(3)
+    sem.release()
+    sem.release()
+    print(sem.__dict__)
