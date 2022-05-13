0b4acba9c75db2d116ff031b639e116e873bc7fd
luof <523274366@qq.com>
2022-05-13 23:21:35 +0800

    [learn]git

---
+++ chapter6_exception_and_module/git_and_create_version_mgmt_server.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""
+@author: f.l
+@time: 2022/5/13
+@File: git_and_create_version_mgmt_server.py
+
+
+
+一、搭Gogs私服：
+    项目不同时期使用了不同python版本开发，假如要回到之前的某一个版本怎么办。又或者很多人同时开发一个项目，避免代码冲突、覆盖。所以需要版本管理，
+    版本管理-即快照，以下记录搭gogs版本管理服务器。
+
+    需要Mysql。一般用rpm安装，或者用二进制发行版。
+    比较流行的Mysql版本：Percona-Server***.tar
+    安装方式：至少安装shared、client、Server，且按shared -> client -> Server顺序安装：
+    Percona-Server***.tar安装博客参考：https://blog.csdn.net/m0_37461645/article/details/83116326
+
+    1、安装完
+    ss-tanl 查看端口监听，mysql端口3306
+    ls /etc/init.d/my* 查看装上没有(名字：mysql）
+    启动、停止：service mysql start/stop
+
+    2、启动是设置环境
+    启动之前cmd命令行执行`/usr/bin/mysql_secure_installation`，测试mysql服务起来没有，并设置基本环境
+    执行时，报ERROR***/mysql.sock时，表示mysql服务没起来
+    `/usr/bin/mysql_secure_installation`：设置root密码、要不要匿名用户anonymous、允不允许root密码远程登录(一般允许，方便windows登录)等等。
+    3、进入后：
+    执行 show databases；
+
+二、git：
+    多人并行协作开发。同时修改一个函数，修改不一样，合并的时候遇到冲突怎办。不同人提交的修改就是分支，多个分支最后合并，通过版本管理，否则并行开发乱套了。
+
+概念：
+    **repository，版本库**：分为远程仓库和本地仓库，要管理的文件都放到仓库里。`git init`初始化后，会在当前目录生成一个`.git`目录，这个目录就是版本库
+
+    **工作区，workspace**：`.git`所在的目录就是工作区，一般时项目的根目录。注意：工作区不是指`.git`目录，是指`.git`位于的那个目录，比如`.git`位于notes目录，则notes目录内都是工作区，
+    `.git`被隐藏，也不要再`.git`目录内工作，因为搞不好就破坏了版本库，无法和远端库通信。
+
+    **index，索引、暂存区**：介于工作区和版本库之间，暂时缓存修改的，通过`git add filename`，将修改的文件放入index
+    **commit，确认**：`git commit`，将暂存区的目录加入版本库，
+    **remote，远端版本库**：便于多人项目开发，在github等服务器上创建的版本库，可以和本地库交互。通过`git push`，将本地库修改push到远端库，`git pull`拉取远端库从而将修改更新到本地库。
+
+    push：工作区workspace(代码的编辑环境)  -add->  索引index(待提交)  -commit->  本地仓库repository  -push->  remote
+    pull: remote  -pull->  workspace
+
+使用：
+    1、初始化一个版本库：`git init`，执行后，就会在当前目录生产`.git`目录，即版本库。
+    2、添加一个`index.html`文件到暂存区：`git add index.html`；`git add`，不接文件，将所有修改添加到暂存区，不建议这样用，除非清楚的知道自己修改了哪些文件。添加前和添加后，通过通过`git status`，查看文件状态分别为：
+添加前：
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        index.html
+
+添加后：
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   index.html
+
+    也就是说，添加前，**修改过的文件**未被跟踪管理，修改未保存这样不安全，比如好不容易码了一天，不小心删除了，找不回来。添加后，就变成被跟踪管理的文件了，即使被删了，也能从暂存区恢复。
+    总之，通过`git add filename`，把文件**当前变化**增加到暂存区管理起来。暂存后，再次修改可再次暂存，还可以暂存新文件
+    可以通过`git add .` 将当前目录变化的文件批量递归的添加到暂存区。
+
+    3、忽略列表`.gitignore`： 由2，通过`git status`，可以查看当前工作区文件状态，可以看到哪些**修改过的文件**没有被跟踪管理。这里涉及到不需要跟踪的文件，即忽略ignored的文件。
+    `.gitignore`文件就是忽略列表。这个文件不需要自己写，python已经有了。
+
+    4、提交代码`git commit`：比如提交刚刚暂存的`index.html`文件。`git commit -m "First commit" index.html`。
+    注意，一定要加"-m"，即message，后面写修改描述（因为什么问题，修改了哪些文件、哪些函数，等等），便于跟踪管理。项目中不写这个要被干，通过Pycharm commit会弹出一个窗口写message，很方便
+
+    5、提交完后，再次`git status`查看一把:
+$ git status
+On branch master
+nothing to commit, working tree clean
+
+    以上，就完成了从初始化一个版本库，到创建文件，保存修改，再到提交代码的流程。
+
+    6、修改`index.html`，再次执行`git status`，可以看到modified文件，代表当前修改未暂存staged。
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   index.html
+
+    7、将第6步修改，直接通过`git commit -m "First commit"`命令。将报
+no changes added to commit (use "git add" and/or "git commit -a")
+    而通过`git commit -m "First commit" index.html`时，无需执行`git add index.html`，即可提交到版本库
+
+    **小结**：git提交分为两个步骤：
+        1、暂存变更：git add，将新文件或修改添加到暂存区stage，也就是index中
+        2、提交变更：git commit，提交暂存区中的改动，而不是物理文件目前的改动（未提到到暂存区的改动），提交到当前分支（版本库）。
+        3、可以通过：`git commit -m "msg" filename` 或者 `git commit -m "msg" -a`,将暂存stash + 提交commit合为一步。
+
+    8、增补，git commit --amend：
+        场景：问题修改后，所有修改已经commit到版本库了，但是发现漏改了一个文件。因为项目严格要求一个问题应当只有一个commit，最多再多一个检视修改，即两次commit。怎办呢？
+        `git commit --amend`。`--amend`后面什么都不写，默认将修改追加到最后一次commit中。
+         比如遗漏了`about.html`文件，修改后执行`git add about.html;git commit --amend`，此时会弹出文件，让你补充message，直接i,增加补充信息后wq退出。
+$ git commit --amend
+[master e101a8b] Thrid commit,修改index.html文件 --amend 追加修改about.html1
+ Date: Fri May 13 22:40:17 2022 +0800
+ 2 files changed, 2 insertions(+), 1 deletion(-)
+ create mode 100644 about.html
+
+    9、查看每次commit的md5值，`git log`。 git log  e101a8b9cf7，指定md5时，查看指定某次提交的信息。
+
+    10、**查看差异**，`git diff`：查看已被跟踪文件未暂存的修改（工作区）与暂存区之间的差异；
+                `git diff --cached`，查看被跟踪文件，暂存区与版本库之间的差异；
+                `git diff HEAD`，查看被跟踪文件，工作区与上一次commit之间的差异，HEAD指代最后一次commit。
+        **一定要练习试验**
+$ git diff --cached
+diff --git a/about.html b/about.html
+index 7bfdb71..eb55abd 100644
+--- a/about.html
++++ b/about.html
+@@ -1 +1,3 @@
+-<html>about</html>
++<html>about;
++modify
++</html>
+
+    11、HEAD：HEAD指代最后一次提交；`HEAD^`，指代上一次提交；`HEAD^^`,指代上上一次提交；查看前n次提交,索引从0开始：`HEAD~n`。例如`git log HEAD~3`
+
+
+
+
+
+"""
+
+if __name__ == '__main__':
+    pass
